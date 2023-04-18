903bc77cab01ede6dd032e34fe6def16296bd854
Raemil Corniel Francisco <r.corniel@bravo.com.do>
2023-04-18 17:05:17 -0400

    handle states with array

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
+import { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+//continue on video #36 
+
+// actual application 
+class App extends Component {
+  constructor(){
+    super();
+
+    //handle states 
+    this.state= {
+      //every element should have a id, --> each child in a list should have a unique "key" prop. allows to render just the element it needs, no all of them
+      monsters :[
+        {
+          name: "Linda",
+          id: '1'
+        },
+        {
+          name:"Frank",
+          id: '2'
+        },
+        {
+          name : "Pedro", 
+          id: '3'
+        }
+      ]
+    }
+  }
+
+
+render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    { //use of map to iterate our array of monsters 
+      this.state.monsters.map((monster)=> { 
+        return  <h1 key={monster.id}>{monster.name}</h1> ;
+      
+      })
+    }
     </div>
   );
+}
 }
 
 export default App;
