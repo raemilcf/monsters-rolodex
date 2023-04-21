b6edb0ae3e322584467ba97cb9f6dd326421721a
Raemil Corniel Francisco <r.corniel@bravo.com.do>
2023-04-21 16:23:33 -0400

    react strict doble render to catch weird behavior

--- src/index.js
+++ src/index.js
@@ -7,10 +7,14 @@ import reportWebVitals from './reportWebVitals';
 //render in the index.html 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <React.StrictMode> 
     <App />
   </React.StrictMode>
 );
+
+//strict mode doble render everything to catch every weird behavior 
+//impact preformence 
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
